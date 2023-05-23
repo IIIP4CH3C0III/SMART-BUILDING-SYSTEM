c3a1002fb25a0db64d539b0813f5f13ff2d3f9ba
4mmc2001 <mcanastro1@gmail.com>
2023-05-23 15:08:27 +0100

    logs page
    falta alterar o js para a tabela da página de logs

--- code/scripts/script.js
+++ code/scripts/script.js
@@ -185,3 +185,104 @@ function default_values()
       list_box3.selectedIndex = 0;
 }
 
+
+/* scripts para os logs*/
+
+function load_table2() 
+{
+	let rowContainer = document.getElementById("rowContainer");
+	let table = document.createElement("div");
+     table.id = "remove"; // Atribuir o id, para futuramente podermos remover
+     table.classList.add("table_db"); //nome da classe
+	 
+const logrowData ) =[
+	{id:"001", edifício:"1", andar:"1", divisão:"1", estado:"0", descrição:"Janeiro Fernandes", hora:"23:55", data:"13-05-2023", grau:"3"}
+	{id:"002", edifício:"2", andar:"3", divisão:"2", estado:"1", descrição:"Carlos Santos", hora:"08:30", data:"19-04-2023", grau:"2"}
+	{id:"003", edifício:"3", andar:"2", divisão:"4", estado:"0", descrição:"Maria Silva", hora:"14:15", data:"07-06-2023", grau:"1"}
+	{id:"004", edifício:"1", andar:"4", divisão:"3", estado:"1", descrição:"Pedro Costa", hora:"10:45", data:"30-05-2023", grau:"3"}
+	{id:"005", edifício:"2", andar:"1", divisão:"2", estado:"1", descrição:"Ana Rodrigues", hora:"16:20", data:"21-05-2023", grau:"2"}
+	{id:"006", edifício:"3", andar:"3", divisão:"1", estado:"0", descrição:"Ricardo Santos", hora:"09:10", data:"10-04-2023", grau:"1"}	
+	{id:"007", edifício:"1", andar:"2", divisão:"3", estado:"0", descrição:"Lúcia Pereira", hora:"13:40", data:"15-06-2023", grau:"3"}
+	{id:"008", edifício:"2", andar:"4", divisão:"4", estado:"1", descrição:"João Mendes", hora:"11:30", data:"05-05-2023", grau:"2"}
+	{id:"009", edifício:"3", andar:"1", divisão:"1", estado:"0", descrição:"Sara Oliveira", hora:"18:05", data:"27-04-2023", grau:"1"}
+	{id:"010", edifício:"1", andar:"3", divisão:"2", estado:"1", descrição:"Miguel Alves", hora:"07:50", data:"14-05-2023", grau:"3"}
+	{id:"011", edifício:"2", andar:"2", divisão:"4", estado:"0", descrição:"Inês Rodrigues", hora:"15:20", data:"08-06-2023", grau:"2"}
+	{id:"012", edifício:"3", andar:"4", divisão:"3", estado:"1", descrição:"Tiago Sousa", hora:"12:10", data:"31-05-2023", grau:"1"}
+	{id:"013", edifício:"1", andar:"1", divisão:"2", estado:"1", descrição:"Cláudia Silva", hora:"17:45", data:"22-05-2023", grau:"3"}
+	{id:"015", edifício:"3", andar:"2", divisão:"3", estado:"0", descrição:"Ana Oliveira", hora:"09:30", data:"18-06-2023", grau:"2"}
+	{id:"016", edifício:"1", andar:"4", divisão:"1", estado:"1", descrição:"Rui Martins", hora:"14:55", data:"29-05-2023", grau:"3"}
+	{id:"017", edifício:"2", andar:"1", divisão:"3", estado:"1", descrição:"Carolina Pereira", hora:"11:20", data:"20-05-2023", grau:"2"}
+	{id:"018", edifício:"3", andar:"3", divisão:"2", estado:"0", descrição:"André Ferreira", hora:"16:10", data:"09-04-2023", grau:"1"}
+	{id:"019", edifício:"1", andar:"2", divisão:"4", estado:"0", descrição:"Mariana Santos", hora:"12:40", data:"14-06-2023", grau:"3"}
+	{id:"020", edifício:"2", andar:"4", divisão:"1", estado:"1", descrição:"Paulo Lima", hora:"10:30", data:"04-05-2023", grau:"2"}
+	{id:"021", edifício:"3", andar:"1", divisão:"3", estado:"0", descrição:"Isabel Fernandes", hora:"19:05", data:"26-04-2023", grau:"1"}
+	{id:"022", edifício:"1", andar:"3", divisão:"2", estado:"1", descrição:"Daniel Almeida", hora:"08:50", data:"13-05-2023", grau:"3"}
+	{id:"023", edifício:"2", andar:"2", divisão:"4", estado:"0", descrição:"Inês Sousa", hora:"15:30", data:"06-06-2023", grau:"2"}
+	{id:"024", edifício:"3", andar:"4", divisão:"1", estado:"1", descrição:"Luís Mendes", hora:"12:20", data:"29-05-2023", grau:"1"}
+	]
+	
+// buscar informação do numero de linhas que pretendemos ver
+      let listbox_element = document.getElementById("listbox_bottom");
+      let numberChoosed = listbox_element.value;
+
+      // Definir as váriaveis para os diversos objetos filhos
+      let rowText , rowIcons, row, cont=0;
+	
+// Se a quantidade que o utilizador pretender mostrar for superior a quantidade de dados existentes, limitar esses dados aos existentes
+      if(numberChoosed > rowData.length)
+            numberChoosed = rowData.length;
+
+      // Varrer e criar as linhas
+      for (let i = 0; i < numberChoosed ; i++) 
+      {
+            //Criar linha
+            row = document.createElement("div");
+            row.classList.add("row");
+
+            //Criar divisoria para texto
+            rowText = document.createElement("div");
+            rowText.classList.add("row_text");
+
+            //Criar divisoria para icones
+            rowIcons = document.createElement("div");
+            rowIcons.classList.add("row_icons");
+
+            // Prencher campos de acordo com a key -> ex.: "id","nome","..."
+            for (let key in rowData[i]) {
+                  //criar divisoria para o campo
+                  let rowField = document.createElement("div");
+
+                  rowField.setAttribute("id",fields_id[cont]);
+                  cont++;
+
+                  rowField.classList.add("row_field");
+                  rowField.textContent = logrowData[i][key];
+                  rowText.appendChild(rowField);
+            }
+
+            cont = 0;
+
+            // Create icons for each row
+            let editIcon = document.createElement("a");
+            editIcon.classList.add("row_icon");
+            editIcon.href = "#";
+            editIcon.innerHTML = '<i class="fa-solid fa-pen fa-lg"></i>';
+            rowIcons.appendChild(editIcon);
+
+            let deleteIcon = document.createElement("a");
+            deleteIcon.classList.add("row_icon");
+            deleteIcon.href = "#";
+            deleteIcon.innerHTML = '<i class="fa-solid fa-trash-can fa-lg"></i>';
+            rowIcons.appendChild(deleteIcon);
+
+            // Append row text and icons to the rowContainer
+            row.appendChild(rowText);
+            row.appendChild(rowIcons);
+
+            table.appendChild(row);
+      }
+
+      rowContainer.appendChild(table);
+const log_fields_id = ["f1","f2","f3","f4","f5","f6","f7","f8","f9"];
+
+}
