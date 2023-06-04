eb0fa3fcc0e5d1cf45b61e71687112d4039a0591
IIIP4CH3C0III <pacheco.castro.fabio@gmail.com>
2023-06-04 22:08:15 +0100

    Mudanças feitas no style em termos de botões, e mudança de um valor no php file

--- code/scripts/php/checkLogin.php
+++ code/scripts/php/checkLogin.php
@@ -5,8 +5,8 @@
 		$username = $_POST['username'];
 		$password = $_POST['password'];
 
-		$name = 'Maria Chaveiro';
-		$level = '2';
+		$name = 'José Azevedo'; 
+		$level = '1';
 		$session_id = 'IDCONNECTED';
 
 		if($username === 'admin' && $password === 'admin')
