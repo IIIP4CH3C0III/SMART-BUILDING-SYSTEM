7a05726e82587689b1a3ec894281ddd3f29642ad
IIIP4CH3C0III <pacheco.castro.fabio@gmail.com>
2023-06-09 01:48:06 +0100

    Test DB V0.03

--- code/scripts/php/checkLogin.php
+++ code/scripts/php/checkLogin.php
@@ -1,38 +1,56 @@
 <?php
 
-	//se receber um post!
+	//se receber um post! cmd : docker-php-ext-install mysqli
 	if( $_SERVER['REQUEST_METHOD'] === 'POST' )
 	{
 		//capturar os dados enviados pelo javascript
-		$username = $_POST['username'];
-		$password = $_POST['password'];
+		$given_username = $_POST['username'];
+		$given_password = $_POST['password'];
 
 		// Definições da conexão á base de dados
-		$connect = mysqli_connect(
-	        'db',
-	        'admin',
-	        'LaGcd62EpArSTt5B',
-	        'sbs',
-		);
+	    $MYSQL_SERVERNAME = 'db';
+	    $MYSQL_USERNAME = 'admin';
+	    $MYSQL_PASSWORD ='LaGcd62EpArSTt5B'
+	    $MYSQL_DB = 'sbs';
+
+	    // Criar a conexão
+	    $conn = new mysqli($MYSQL_SERVERNAME,$MYSQL_USERNAME,$MYSQL_PASSWORD,$MYSQL_DB);
+
+	    if ( $conn->connect_error )
+	    	die("error_1");
 
 		// Nome da tabela de pesquisa
 		$table_name = "CLIENTS";
 
 		// Query de SQL para procurar pelo username
-		$query = "SELECT * FROM $table_name WHERE username = $username";
+		$sql = "SELECT * FROM $table_name WHERE FULL_NAME = '$given_username'";
 
-		// Pedido na base de dados
-		$response = mysqli_query($connect, $query);
+		// Pedido na base de dados, com sua resposta em result
+		$result = $conn->query($sql);
 
-		//linha de resposta
-		$row = mysqli_fetch_assoc($response);
+		if ( $result->num_rows > 0 )
+		{
+			//guardar em row, a informação contida numa das linhas onde encontrou a informação
+			$row = $result->fetch_assoc();
+			$stored_password = $row["PASSWORD"];
 
-        $name = $row['FULL_NAME'];  
-        $level = $row['LEVEL'];
-        $session_id = $row['ID_SESSON'];
+			if ( $given_password != $stored_password )
+			{
+				$conn->close();
+				die ("error_2");
+			}
 
-        echo "success,${name},${level},${session_id}" ;
 
+			$name = $row['FULL_NAME'];  
+	        $level = $row['LEVEL'];
+	        $session_id = $row['ID_SESSON'];
+
+	        echo "success,${name},${level},${session_id}" ;
+
+		}		
+
+		$conn->close();
 
 	}
 ?>
+
